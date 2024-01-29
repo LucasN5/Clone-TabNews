df1e433b6cd992c4f2edfe535f9b5af24a549bf7
LucasN5 <lucasxben@hotmail.com>
2024-01-29 15:20:30 -0300

    Mudança de frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
     return (
-        <h1>Eu vou virar um progrmador front-end em 2024</h1>
+        <div>
+            <h1>Eu vou virar um progrmador front-end em 2024</h1>
+            <h1>E conseguir um PS5, pelo amor de Deus eu quero mto jogar like a dragon gaiden, like a dragon e infinite wealth AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        </div>
+        
     )
 }
 
