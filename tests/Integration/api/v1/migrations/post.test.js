30000d93bbed830f376b6e59e83b280749892864
LucasN5 <lucasxben@hotmail.com>
2024-04-18 15:38:41 -0300

    Adiciona /api/v1/migrations endpoint

---
+++ tests/Integration/api/v1/migrations/post.test.js
@@ -0,0 +1,31 @@
+import database from "infra/database.js";
+
+beforeAll(clearDatabase);
+
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("Método POST vai retornar 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const responseBody = await response1.json();
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const responseBody2 = await response2.json();
+  console.log(responseBody2);
+
+  expect(Array.isArray(responseBody2)).toBe(true);
+  expect(responseBody2.length).toBe(0);
+});
